c32968b84c4b23ce2e7ab4fb001901d4755b6ee2
Mayank Pratap Singh <48886579+mayankprsingh@users.noreply.github.com>
2020-08-13 22:45:50 +0530

    Session-1

---
+++ Session-1/src/MyApps.java
@@ -0,0 +1,12 @@
+
+public class MyApps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is Awesome");
+		System.out.println("Lets meet at 5PM Now :)");
+
+	}
+
+}
